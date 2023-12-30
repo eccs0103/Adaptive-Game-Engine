02e36749260d5af7afcdce7d029a20dba48b2e0d
Arman <eccs0103@gmail.com>
2023-12-30 20:45:30 +0400

    1.0.0

--- Scripts/Client/Main.js
+++ Scripts/Client/Main.js
@@ -3,15 +3,13 @@
 import { Node, canvas, context, display, progenitor } from "./Components/Node.js";
 import { } from "./Structure.js";
 
-void async function () {
-	try {
-		progenitor.addEventListener(`start`, (event) => {
-			// Program start callback
-		});
-		progenitor.addEventListener(`update`, (event) => {
-			// Frame update callback
-		});
-	} catch (error) {
-		await window.prevent(document.analysis(error));
-	}
-}();
+try {
+	progenitor.addEventListener(`start`, (event) => {
+		// Program start callback
+	});
+	progenitor.addEventListener(`update`, (event) => {
+		// Frame update callback
+	});
+} catch (error) {
+	await window.prevent(document.analysis(error));
+}
