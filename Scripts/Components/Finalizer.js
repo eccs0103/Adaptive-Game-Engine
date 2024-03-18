fe084337784a45231ae6a414eadcc9960d0bc519
Arman <eccs0103@gmail.com>
2024-03-18 21:39:25 +0400

    1.3.1

--- Scripts/Components/Finalizer.js
+++ Scripts/Components/Finalizer.js
@@ -1,12 +1,12 @@
 "use strict";
 
-import { display, progenitor } from "./Node.js";
+import { engine, progenitor } from "./Node.js";
 import { Renderer } from "./Utilities.js";
 
 progenitor.addEventListener(`update`, (event) => {
 	Renderer.clear();
 	progenitor.dispatchEvent(new Event(`render`, { bubbles: true }));
 });
-display.launched = true;
+engine.launched = true;
 
 export { };
